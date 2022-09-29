8591ffdca24369c2736dc17215ff9a15a83b1c9b
ngocnhat2k1 <1951120116@sv.ut.edu.vn>
2022-09-29 16:58:56 +0700

    complete navbar

--- client/src/components/Header/NavBar/NavBar.js
+++ client/src/components/Header/NavBar/NavBar.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 import LogoSrc from '../../../images/Logo.png'
 import { HomeDropDown, NavBarItems, FeatureDropDown, ShopDropDown, BlogDropDown, PagesDropDown } from './NavBarItems.js'
 import "./NavBar.css";
-import { Link, BrowserRouter as Router } from 'react-router-dom';
+import { Link, } from 'react-router-dom';
 import { FaHeart, FaShoppingBag, FaSearch } from "react-icons/fa";
 import DropDown from "./Dropdown.js";
 
@@ -22,7 +22,6 @@ function NavBar() {
 
   const ShowNavBar = () => {
     navRef.current.classList.toggle("responsive_nav")
-    console.log("dã dính")
   }
 
   return (
@@ -39,100 +38,90 @@ function NavBar() {
               </div>
             </div>
 
-            <div
+            <ul className='nav-item-ul'
               ref={navRef}
             >
-              <ul className='nav-item-ul  '>
-                {NavBarItems.map((item) => {
-                  if (item.title === "Home") {
-                    return (
-                      <li
-                        key={item.id}
-                        className={item.cName}
-                        onMouseEnter={() =>
-                          setHomeDropdown(true)
-                        }
-                        onMouseLeave={() => setHomeDropdown(false)}
-                      >
-                        <Link to={item.path}
-
-                        >{item.title}</Link>
-                        <DropDown
-                          className={`${HomeDropdown ? 'active' : 'dropdown'}`}
-                          nameDropDown={HomeDropDown} />
-                      </li>
-                    );
-                  }
-                  else
-                    if (item.title === "Shop") {
-                      return (
-                        <li
-                          key={item.id}
-                          className={item.cName}
-                          onMouseEnter={() => setShopDropdown(true)}
-                          onMouseLeave={() => setShopDropdown(false)}
-                        >
-                          <Link to={item.path}>{item.title}</Link>
-                          <DropDown className={`${ShopDropdown ? 'active' : 'dropdown'}`}
-                            nameDropDown={ShopDropDown} />
-                        </li>
-                      );
-                    }
-                    else
-                      if (item.title === "Feature") {
-                        return (
-                          <li
-                            key={item.id}
-                            className={item.cName}
-                            onMouseEnter={() => setFeatureDropdown(true)}
-                            onMouseLeave={() => setFeatureDropdown(false)}
-                          >
-                            <Link to={item.path}>{item.title}</Link>
-                            <DropDown className={`${FeatureDropdown ? 'active' : 'dropdown'}`}
-                              nameDropDown={FeatureDropDown} />
-                          </li>
-                        )
-                      }
-                      else
-                        if (item.title === "Blog") {
-                          return (
-                            <li
-                              key={item.id}
-                              className={item.cName}
-                              onMouseEnter={() => setBlogDropdown(true)}
-                              onMouseLeave={() => setBlogDropdown(false)}
-                            >
-                              <Link to={item.path}>{item.title}</Link>
-                              <DropDown className={`${BlogDropdown ? 'active' : 'dropdown'}`}
-                                nameDropDown={BlogDropDown} />
-                            </li>
-                          )
-                        }
-                        else
-                          if (item.title === "Pages") {
-                            return (
-                              <li
-                                key={item.id}
-                                className={item.cName}
-                                onMouseEnter={() => setPagesDropdown(true)}
-                                onMouseLeave={() => setPagesDropdown(false)}
-                              >
-                                <Link to={item.path}>{item.title}</Link>
-                                <DropDown className={`${PageDropdown ? 'active' : 'dropdown'}`}
-                                  nameDropDown={PagesDropDown} />
-                              </li>
-                            )
-                          }
-                })}
-                <button
-                  className='nav-btn nav-close-btn'
-                  onClick={ShowNavBar}
-                >
-                  <FaTimes />
-                </button>
-              </ul>
-
-            </div>
+              {NavBarItems.map((item) => {
+                if (item.title === "Home") {
+                  return (
+                    <li
+                      key={item.id}
+                      className={item.cName}
+                      onMouseEnter={() => setHomeDropdown(true)}
+                      onMouseLeave={() => setHomeDropdown(false)}
+                    >
+                      <Link to={item.path}>{item.title}</Link>
+                      <DropDown
+                        className={`${HomeDropdown ? 'active' : 'dropdown'}`}
+                        nameDropDown={HomeDropDown} />
+                    </li>
+                  );
+                }
+                if (item.title === "Shop") {
+                  return (
+                    <li
+                      key={item.id}
+                      className={item.cName}
+                      onMouseEnter={() => setShopDropdown(true)}
+                      onMouseLeave={() => setShopDropdown(false)}
+                    >
+                      <Link >{item.title}</Link>
+                      <DropDown
+                        className={`${ShopDropdown ? 'active' : 'dropdown'}`}
+                        nameDropDown={ShopDropDown} />
+                    </li>
+                  );
+                }
+                if (item.title === "Feature") {
+                  return (
+                    <li
+                      key={item.id}
+                      className={item.cName}
+                      onMouseEnter={() => setFeatureDropdown(true)}
+                      onMouseLeave={() => setFeatureDropdown(false)}
+                    >
+                      <Link >{item.title}</Link>
+                      <DropDown className={`${FeatureDropdown ? 'active' : 'dropdown'}`}
+                        nameDropDown={FeatureDropDown} />
+                    </li>
+                  )
+                }
+                if (item.title === "Blog") {
+                  return (
+                    <li
+                      key={item.id}
+                      className={item.cName}
+                      onMouseEnter={() => setBlogDropdown(true)}
+                      onMouseLeave={() => setBlogDropdown(false)}
+                    >
+                      <Link >{item.title}</Link>
+                      <DropDown className={`${BlogDropdown ? 'active' : 'dropdown'}`}
+                        nameDropDown={BlogDropDown} />
+                    </li>
+                  )
+                }
+                if (item.title === "Pages") {
+                  return (
+                    <li
+                      key={item.id}
+                      className={item.cName}
+                      onMouseEnter={() => setPagesDropdown(true)}
+                      onMouseLeave={() => setPagesDropdown(false)}
+                    >
+                      <Link>{item.title}</Link>
+                      <DropDown className={`${PageDropdown ? 'active' : 'dropdown'}`}
+                        nameDropDown={PagesDropDown} />
+                    </li>
+                  )
+                }
+              })}
+              <button
+                className='nav-btn nav-close-btn'
+                onClick={ShowNavBar}
+              >
+                <FaTimes />
+              </button>
+            </ul>
 
             <ul className="ActionNavBar">
               <li> <a href="."><FaHeart fontSize={21} /></a></li>
