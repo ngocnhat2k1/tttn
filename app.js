0f5959cafb9b0190183d0bd675bdad1874aca882
ngocnhat2k1 <1951120116@sv.ut.edu.vn>
2022-09-30 22:51:00 +0700

    init vendor

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'I am a message from Server!'});
-})
-
-app.listen(4000, () => console.log('App listening on port 4000'));
