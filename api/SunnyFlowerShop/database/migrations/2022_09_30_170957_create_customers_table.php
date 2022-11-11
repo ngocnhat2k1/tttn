72a630b2fc3f939c73566fc4e62d6c399d138be7
Phong Kim Do <94376574+Phongdo1104@users.noreply.github.com>
2022-11-11 23:41:38 +0700

    Changed "default_avatar" value for Customer

--- api/SunnyFlowerShop/database/migrations/2022_09_30_170957_create_customers_table.php
+++ api/SunnyFlowerShop/database/migrations/2022_09_30_170957_create_customers_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string("password");
             $table->longText("avatar")->nullable();
             // this "default_avatar" column is temporary
-            $table->string("default_avatar")->default("https://styles.redditmedia.com/t5_1p1y2i/styles/profileIcon_3ryimhk1cpm41.jpg?width=256&height=256&frame=1&crop=256:256,smart&s=f611c9c0634b41165820ab80c11b8229d1398945");
+            $table->string("default_avatar")->default("https://i.kym-cdn.com/photos/images/facebook/001/921/416/50b");
             // $table->string("phone_number");
             $table->boolean("subscribed")->comment("SubscribeValueEnum");
             $table->boolean("disabled")->nullable()->comment("Default value is NULL, 0 for disable");
