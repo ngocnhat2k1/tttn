3fce2862a9ffbe060a573c20970e34aa27951a45
ngocnhat2k1 <1951120116@sv.ut.edu.vn>
2022-12-20 07:59:36 +0700

    change icon and title in website

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Helmet } from "react-helmet";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+    <Helmet defer={false} >
+      <meta charSet="utf-8" />
+      <title >Hướng Dương Shop</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+    </Helmet>
+    <App />
   </React.StrictMode>
 );
 
